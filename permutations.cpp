096876d8f9025198e67093d43abd0e6f99cb4fde
Manu Chandel <manuchandel@users.noreply.github.com>
2015-08-15 13:16:32 +0530

    Update permutations.cpp

--- permutations.cpp
+++ permutations.cpp
@@ -1,7 +1,7 @@
 /*
  *Generate All possible ppermutations of a string
  */
- 
+
 /* utility function that swaps two characters */
 void swap(char &a,char &b){
   char c=a;
@@ -9,25 +9,22 @@ void swap(char &a,char &b){
   b=c;
 }
 /* generates all possible permutations of the string */
-void generatePermutations(string s,string temp,vector<string> &v,int l){
-  if(l==s.length()){
-    v.push_back(temp);
+void generatePermutations(string &s,vector<string> &v,int l){
+  if(l==s.length()-1){
+    v.push_back(s);
     return;
   }
-  temp+="l";
-  int k=temp.length();
-  for(int i=l+1;i<=s.length();i++){
-    temp[k-1]=s[l];
-    generatePermutations(s,temp,v,l+1);
+  for(int i=l;i<s.length();i++){
+    swap(s[l],s[i]);
+    generatePermutations(s,v,l+1);
     swap(s[l],s[i]);
   }
 }
 /* returns all possible permutations of a string */
-vector<string> permutations(string s){
+vector<string> permute(string s){
   vector<string> v;
-  generatePermutations(s,"",v,0);
+  generatePermutations(s,v,0);
   for(int i=0;i<v.size();i++)
-    cout<<v[i]<<" ";
-  cout<<endl;
+    cout<<v[i]<<endl;
   return v;
 }
